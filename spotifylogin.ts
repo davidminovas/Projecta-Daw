2b7e469858f8a3c1e776508a316a573c2ed534f9
davidminovas <davidminovasbizarro@gmail.com>
2026-09-17 19:39:40 +0200

    Add files via upload

--- spotifylogin.ts
+++ spotifylogin.ts
@@ -1,15 +1,43 @@
-function autentificar(correu: string, contrasenya: string, edat: number): boolean {
-    if (correu === "") {
-        console.log("Error: el camp del correu està buit.");
-    }
-
-    if (contrasenya === "") {
-        console.log("Error: el camp de la contrasenya està buit.");
-    }
-
-    if (edat <= 14) {
-        console.log("Error: has de tenir més de 14 anys per autentificar-te.");
-    }
-
-    return correu !== "" && contrasenya !== "" && edat > 14;
+interface Usuari {
+    contrasenya: string;
+    correu: string;
+    edat: number;
 }
+
+
+function autentificar(usuari: Usuari): boolean {
+    let correcte: boolean = false;
+    if (usuari.correu === "totcorrecta" && usuari.contrasenya === "contrasenyaok" && usuari.edat > 14) {
+        correcte = true;
+    }
+    return correcte;
+
+
+}
+
+function imprimir(auth: boolean) {
+    if (auth) {
+        console.log("Usuari identificat de forma correcta")
+    } else {
+        console.log("Usuari, contrasenya o edat no valid")
+    }
+}
+const usuariCorrecte: Usuari = {
+    contrasenya: "contrasenyaok",
+    correu: "totcorrecta",
+    edat: 15
+
+}
+let autOk: boolean = autentificar(usuariCorrecte);
+
+imprimir(autOk);
+
+const usuariinCorrecte: Usuari = {
+    contrasenya: "contrasenyaok",
+    correu: "totcorrecta",
+    edat: 15
+
+}
+autOk = autentificar(usuariinCorrecte);
+
+imprimir(autOk);
