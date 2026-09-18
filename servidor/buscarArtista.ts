6ff305d4dfdf9e30a3e4fbdbf819a29f63a1b05c
david minovas <dminovas08@boscdelacoma.cat>
2026-09-18 17:47:38 +0200

    resolucio

--- servidor/buscarArtista.ts
+++ servidor/buscarArtista.ts
@@ -1,5 +1,34 @@
 //utilitzan el filter returnar totes les cancos d'un determinat artista
 
+interface Canco {
+    titol: string;
+    artista: string;
+    durada: number;
+    id: string;
+}
+const cancoBuscar: Canco = {
+    id: "2B-CA",
+    titol: "Antes de que cuente diez",
+    artista: "Fito",
+    durada: 300
+};
+
+const cancons: Canco[] = [
+    {
+        id: "2B-CA",
+        titol: "Antes de que cuente diez",
+        artista: "Fito",
+        durada: 300
+    },
+    {
+        id: "3E-DF",
+        titol: "Mustafa",
+        artista: "Bull",
+        durada: 249
+    }
+]
+
+
 function buscarCancoArtista(artista: string, cancoList: Canco[]): Canco[] {
     return cancoList.filter(canco => canco.artista === artista);
 }
@@ -7,4 +36,3 @@ function buscarCancoArtista(artista: string, cancoList: Canco[]): Canco[] {
 const canconsArtista: Canco[] = buscarCancoArtista("Fito", cancons);
 
 console.log(canconsArtista);
-export { };
--- servidor/buscarLlistes.ts
+++ servidor/buscarLlistes.ts
@@ -14,61 +14,90 @@ interface Usuari {
     nom: string;
     llistes: LlistaReproduccio[];
 }
-
-function buscarCanconsArtista(usuari: Usuari, artista: string): Canco[] {
-    const cancons: Canco[] = [];
-
-    for (const llista of usuari.llistes) {
-        const canconsArtista = llista.cancons.filter(
-            canco => canco.artista === artista
-        );
-
-        cancons.push(canconsArtista);
+function songsSerchArtist(artist: string, songs: Canco[]): Canco[] {
+    return songs.filter(
+        (s: Canco) => { return artist = s.artista }
+    )
+};
+function songsPlayList(artista: string, playLists: LlistaReproduccio[]): Canco[] {
+    let songs: Canco[] = [];
+    for (let i: number = 0; i < playLists.length; i++) {
+        const songsArtistas: Canco[] = songsSerchArtist(artista, playLists[i].cancons);
+        songs = songs.concat(songsArtistas);
     }
+    return songs;
+}
+function buscarCanconsArtista(usuarinom: string, artista: string, users: Usuari[]) {
+    const user: Usuari[] = users.filter(
+        (u: Usuari) => { return u.nom === usuarinom }
+    );
+    const playListUser: LlistaReproduccio[] = user[0].llistes;
+    return songsPlayList(artista, playListUser)
 
-    return cancons;
 }
 
-const usuari: Usuari = {
-    nom: "David",
-    llistes: [
-        {
-            nom: "Preferides",
-            cancons: [
-                {
-                    id: "1",
-                    titol: "Antes de que cuente diez",
-                    artista: "Fito",
-                    durada: 300
-                },
-                {
-                    id: "2",
-                    titol: "Mustafa",
-                    artista: "Bull",
-                    durada: 249
-                }
-            ]
-        },
-        {
-            nom: "Rock",
-            cancons: [
-                {
-                    id: "3",
-                    titol: "Soldadito Marinero",
-                    artista: "Fito",
-                    durada: 270
-                },
-                {
-                    id: "4",
-                    titol: "Another Song",
-                    artista: "Queen",
-                    durada: 250
-                }
-            ]
-        }
-    ]
-};
 
-const resultat = buscarCanconsArtista(usuari, "Fito");
+const usuari: Usuari[] = [
+    {
+        nom: "David",
+        llistes: [
+            {
+                nom: "Preferides",
+                cancons: [
+                    {
+                        id: "1",
+                        titol: "Antes de que cuente diez",
+                        artista: "Fito",
+                        durada: 300
+                    },
+                    {
+                        id: "2",
+                        titol: "Mustafa",
+                        artista: "Bull",
+                        durada: 249
+                    }
+                ]
+            },
+            {
+                nom: "Rock",
+                cancons: [
+                    {
+                        id: "3",
+                        titol: "Soldadito Marinero",
+                        artista: "Fito",
+                        durada: 270
+                    },
+                    {
+                        id: "4",
+                        titol: "Another Song",
+                        artista: "Queen",
+                        durada: 250
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        nom: "Joan",
+        llistes: [
+            {
+                nom: "Favoritas",
+                cancons: [
+                    {
+                        id: "4",
+                        titol: "Una cançó",
+                        artista: "Fito",
+                        durada: 280
+                    }
+                ]
+            }
+        ]
+    }
+];
+let nameArtista: string = "Fito"
+let nameUser: string = "David"
 
-console.log(resultat);
+const songs: Canco[] = buscarCanconsArtista(nameUser, nameArtista, usuari);
+
+console.log(songs);
+export { };
